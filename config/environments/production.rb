fe7a62c73af5b12f5148e68ef9ff393a7c82236f
Elliot Nash <Elliot@VentureMedias.com>
2010-03-16 20:45:45 -0400

    update host for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,9 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
+config.action_mailer.default_url_options = { :host => 'http://gentle-planet-26.heroku.com' }
+
+
 # See everything in the log (default is :info)
 # config.log_level = :debug
 
