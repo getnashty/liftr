300d6635307fff41fd7de6d6ba8c5101226b6bf6
Elliot Nash <Elliot@VentureMedias.com>
2010-03-18 23:01:11 -0400

    remove comma

--- vendor/gems/comma-0.3.2/lib/comma/array.rb
+++
@@ -1,5 +0,0 @@
-class Array
-  def to_comma(style = :default)
-    Comma::Generator.new(self, style).run(:each)
-  end
-end
--- vendor/gems/comma-0.3.2/lib/comma/named_scope.rb
+++
@@ -1,6 +0,0 @@
-class ActiveRecord::NamedScope::Scope
-  def to_comma(style = :default)
-    Comma::Generator.new(self, style).run(:find_each)
-  end
-end
-
