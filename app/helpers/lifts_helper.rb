30f314b67928106814609d33d2645cef0fbe15a8
Elliot Nash <Elliot@VentureMedias.com>
2010-04-20 22:45:40 -0400

    update code for input lifts

--- app/helpers/lifts_helper.rb
+++ app/helpers/lifts_helper.rb
@@ -16,19 +16,5 @@ module LiftsHelper
   end
     f.select :exercise_id, options
   end
-
-
-  def exercise_select2(f)
-    options = {}
-    options['Bench Press'] = 1 unless @a == 1
-    options['Squats'] = 2 unless @b == 1
-    options['Deadlifts'] = 3 unless @c == 1
-    options['Rows'] = 4 unless @d == 1
-    options['Tricep Extension'] = 5 unless @e == 1
-    options['Bicep Curl'] = 6 unless @f == 1
-    options['Military Press'] = 7 unless @g == 1
-    f.select :exercise_id, options
-  end
-  
   
 end
