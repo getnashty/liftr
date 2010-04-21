41143932f85bf80eb65e5464f40b5da15f3c1f0d
Elliot Nash <Elliot@VentureMedias.com>
2010-04-20 22:37:30 -0400

    update code for select menu

--- app/helpers/lifts_helper.rb
+++ app/helpers/lifts_helper.rb
@@ -2,6 +2,24 @@ module LiftsHelper
   
   def exercise_select(f)
     options = {}
+    today=Time.now.strftime("%m/%d/%Y") 
+    @exercises.each do |exercise| 
+      noway=0
+    if exercise.name != "Body Weight"
+    @lifts.each do |lift| 
+     if lift.user_id == current_user.id && lift.created_at.strftime("%m/%d/%Y")==today && lift.exercise_id == exercise.id && exercise.name != "Body Weight"
+        noway=1
+      end
+    end
+    options[exercise.name] = exercise.id unless noway == 1
+    end
+  end
+    f.select :exercise_id, options
+  end
+
+
+  def exercise_select2(f)
+    options = {}
     options['Bench Press'] = 1 unless @a == 1
     options['Squats'] = 2 unless @b == 1
     options['Deadlifts'] = 3 unless @c == 1
@@ -11,5 +29,6 @@ module LiftsHelper
     options['Military Press'] = 7 unless @g == 1
     f.select :exercise_id, options
   end
+  
   
 end
