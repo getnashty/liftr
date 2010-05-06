68b15c03e2f1f80ecd1dae2b5eebcb5e115626ff
Elliot Nash <Elliot@VentureMedias.com>
2010-05-05 22:51:32 -0400

    update lift edit

--- app/helpers/lifts_helper.rb
+++ app/helpers/lifts_helper.rb
@@ -47,4 +47,25 @@ module LiftsHelper
   end
   
   
+  def exercise_edit(f)
+    options = {}
+    today=Time.now.strftime("%m/%d/%Y") 
+            
+    @exercises.each do |exercise| 
+      noway=0
+      
+    if exercise.name != "Body Weight"
+    @lifts.each do |lift| 
+     if lift.user_id == current_user.id && lift.created_at.strftime("%m/%d/%Y")==today && lift.exercise_id == exercise.id && exercise.name != "Body Weight"
+        noway=1
+      end
+    end
+    options[exercise.name] = exercise.id unless noway == 1
+    end
+  end
+  
+    f.select :exercise_id, options
+  end
+  
+  
 end
