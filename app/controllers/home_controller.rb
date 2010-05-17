b1ac6918e4fd864bba7be25dffa5d2fc10ddc3ab
Elliot Nash <Elliot@VentureMedias.com>
2010-05-16 22:19:00 -0400

    community statz

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,21 +7,11 @@ def index
     @users = User.find(:all) 
     @usernum = User.count
     @liftnum = Lift.count
-    @index_data_link = formatted_index_home_url(:xml)
     respond_to do |format|
       format.html
       format.xml  { render :action => "index.xml.builder", :layout => false }
     end
   end
-  
-  def comm
-      @lifts = Lift.find(:all, :order => 'created_at') 
-      @exercises = Exercise.all
-      @comm_data_link = formatted_comm_home_url(:xml)
-      respond_to do |format|
-        format.html
-        format.xml  { render :action => "comm.xml.builder", :layout => false }
-      end
-    end
+
   
 end
