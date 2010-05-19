040d590a05796ad1cb5d73f9809ebd307a95421d
Elliot Nash <Elliot@VentureMedias.com>
2010-05-18 22:48:03 -0400

    update lifts controlla

--- app/controllers/lifts_controller.rb
+++ app/controllers/lifts_controller.rb
@@ -5,8 +5,9 @@ class LiftsController < ApplicationController
   def index
     
     #@lifts = Lift.paginate_by_user_id current_user.id, :page => params[:page], :order => 'created_at DESC' 
-    @lifts = Lift.find_all_by_user_id(current_user.id, :order => 'created_at') 
-    @todaylifts = Lift.find_all_by_user_id(current_user.id, :order => 'created_at')     
+    @lifts = Lift.find_all_by_user_id(current_user.id, :order => 'created_at DESC') 
+    #@lifts=current_user.lifts(:order => 'created_at DESC', :include => :exercise)
+    @todaylifts = Lift.find_all_by_user_id(current_user.id, :order => 'created_at DESC')     
     
     @lift = Lift.new
     @exercises = Exercise.all
